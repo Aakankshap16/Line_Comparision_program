d412cd7597294ad9480f59a88474b9c03ae8d81c
Aakankshap16 <aakankshap971@gmail.com>
2023-03-10 23:09:09 +0530

    with uc1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using LineComparison;
+using System;
 
 namespace LineComparision
 {
     class Program
         {
           public static void Main(string[] args)
-        {
+          {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
-        }
+            CalculateLength.call();
+          }
         }
 }
